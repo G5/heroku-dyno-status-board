47b3475c202f39dbe124e5d68007e7b1d57120fd
Jessica Dillon <jessicard@mac.com>
2013-07-08 23:58:39 -0700

    Adds OmniAuth Heroku strategy

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
-source "http://rubygems.org"
+source 'http://rubygems.org'
 
-gem "json"
-gem "sinatra"
+gem 'json'
+gem 'sinatra'
+gem 'omniauth'
+gem 'omniauth-heroku'
+gem 'heroku-api'
+gem 'thin'
+gem 'dotenv-rails', :groups => [:development, :test]
--- server.rb
+++ server.rb
@@ -1,10 +1,38 @@
+require 'thin'
 require 'sinatra'
+require 'omniauth'
+require 'omniauth-heroku'
+require 'heroku-api'
 
 module G5
   class HerokuDynoStatusBoard < Sinatra::Base
 
+    configure do
+      enable :sessions
+    end
+
+    use OmniAuth::Builder do
+      provider :heroku, ENV['HEROKU_OAUTH_ID'], ENV['HEROKU_OAUTH_SECRET']
+    end
+
+
     get '/' do
-      "Hello World"
+      '<a href="/sign_in">Sign in with Heroku</a>'
+    end
+
+    get '/sign_in' do
+      redirect '/auth/heroku'
+    end
+
+    get '/auth/:name/callback' do
+      auth = request.env['omniauth.auth']
+      access_token = request.env['omniauth.auth']['credentials']['token']
+      heroku_api = Heroku::API.new(:api_key => access_token)
+      @apps = heroku_api.get_apps.body
+
+      puts @apps
+
+      redirect '/'
     end
 
   end
