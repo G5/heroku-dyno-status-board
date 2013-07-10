732b12182ebd961929f971e70c556fa03a71ec40
Jessica Dillon <jessicard@mac.com>
2013-07-10 15:58:53 -0700

    creates ember app and adds heroku_apps routes & models

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,24 @@
-// var App = Ember.Application.create();
+var App = Ember.Application.create();
+
+// Router
+App.Router.map(function() {
+  this.resource('heroku_apps', function(){
+    this.resource('heroku_app', { path: ':heroku_app_id' });
+  });
+});
+
+App.HerokuAppsRoute = Ember.Route.extend({
+  model: function() {
+    return App.HerokuApp.find();
+  }
+});
+
+// Controllers
+
+
+// Models
+App.Store = DS.Store.extend();
+
+App.HerokuApp = DS.Model.extend({
+  name: DS.attr('string')
+});
